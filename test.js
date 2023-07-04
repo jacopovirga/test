f675a8bd04b2ed1fec79830bb3dd5318f6c51252
jacopovirga <77298894+jacopovirga@users.noreply.github.com>
2023-07-04 07:12:45 -0700

    a

--- test.js
+++ test.js
@@ -1,7 +1,2 @@
-function calculateDaysBetweenDates(date1, date2) {
-  const difference = date2.getTime() - date1.getTime()
-  return Math.floor(difference / (1000 * 3600 * 24))
-}
-
-//return the current time
-function getCurrentTime() {
+// hello world program
+// console.log("Hello World");
