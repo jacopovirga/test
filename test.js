7de56d8219e0328f7825d2f8010ee3a4cde615bb
jacopovirga <77298894+jacopovirga@users.noreply.github.com>
2023-07-04 07:09:45 -0700

    updated js

--- test.js
+++ test.js
@@ -1,1 +1,7 @@
-function calculateDaysBetweenDates(begin, end)
+function calculateDaysBetweenDates(date1, date2) {
+  const difference = date2.getTime() - date1.getTime()
+  return Math.floor(difference / (1000 * 3600 * 24))
+}
+
+//return the current time
+function getCurrentTime() {
